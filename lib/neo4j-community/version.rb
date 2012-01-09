bba7ebdb9ce5de08b7b5c927b761d8b1de08a3cf
Dmytrii Nagirniak <dnagir@gmail.com>
2012-01-09 21:55:41 +1100

    forgot file

--- lib/neo4j-community/version.rb
+++ lib/neo4j-community/version.rb
@@ -1,5 +1,8 @@
 module Neo4j
   module Community
+    # dnagir: I don't think gem version should == NEO_VERSION
+    # It allows updating gem without neo4j itself
     VERSION = "0.0.1.alpha"
+    NEO_VERSION = "1.6.M02"
   end
 end
