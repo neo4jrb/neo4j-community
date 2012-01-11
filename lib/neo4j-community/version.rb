a1dd6c257c4ee92af03648402be93f27abcfcd17
Andreas Ronge <andreas.ronge@gmail.com>
2012-01-11 15:55:48 +0100

    make sure there is no conflict between different neo4j jar files

--- lib/neo4j-community/version.rb
+++ lib/neo4j-community/version.rb
@@ -1,6 +1,14 @@
 module Neo4j
+
   module Community
     VERSION = "1.6.0.alpha.3"
     NEO_VERSION = "1.6.M02"
+  end
+
+  # make sure community, advanced and enterprise neo4j jar files have the same version
+  if defined?(NEO_VERSION) && NEO_VERSION != Community::NEO_VERSION
+    raise "Mismatch of Neo4j JAR versions. Already loaded JAR files #{NEO_VERSION}, neo4j-community: #{Community::NEO_VERSION}" 
+  else
+    NEO_VERSION = Community::NEO_VERSION
   end
 end
