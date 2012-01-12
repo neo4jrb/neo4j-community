e5dc9e0b2a8846fcef17e2829586d8dc32b7d77c
Dmytrii Nagirniak <dnagir@gmail.com>
2012-01-13 09:40:09 +1100

    provide common ways for dependent gems to ensure jars version

--- lib/neo4j-community.rb
+++ lib/neo4j-community.rb
@@ -13,6 +13,13 @@ module Neo4j
       Dir["#{jars_root}/*.jar"].each {|jar| require jar }
     end
 
+    # This can be used by dependent gems to verify the Database versions have no mismatch.
+    def self.ensure_version!(other, edition)
+      matching = ::Neo4j::Community::NEO_VERSION == other
+      return unless matching
+      raise "Mismatch of Neo4j JAR versions. Already loaded JAR files #{::Neo4j::Community::NEO_VERSION}, neo4j-#{edition}: #{other}." 
+    end
+
   end
 end
 
