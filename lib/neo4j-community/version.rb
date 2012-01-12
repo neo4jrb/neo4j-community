05d45c82bfe33ba2d58a80ce9f08465e2d9b956f
Dmytrii Nagirniak <dnagir@gmail.com>
2012-01-13 09:20:21 +1100

    don't try to reflect on dependent gem. Should be the other way around.

--- lib/neo4j-community/version.rb
+++ lib/neo4j-community/version.rb
@@ -5,12 +5,4 @@ module Neo4j
     NEO_VERSION = "1.6.M02"
   end
 
-  # make sure community, advanced and enterprise neo4j jar files have the same version
-  if defined?(NEO_VERSION)
-    if NEO_VERSION != Community::NEO_VERSION
-      raise "Mismatch of Neo4j JAR versions. Already loaded JAR files #{NEO_VERSION}, neo4j-community: #{Community::NEO_VERSION}" 
-    end
-  else
-    NEO_VERSION = Community::NEO_VERSION
-  end
 end
