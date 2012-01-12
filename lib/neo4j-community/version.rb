c9c0b6736741b5b5974fd93379573077e89bfd4f
Dmytrii Nagirniak <dnagir@gmail.com>
2012-01-13 09:30:54 +1100

    expose NEO_VERSION globally. One and only here

--- lib/neo4j-community/version.rb
+++ lib/neo4j-community/version.rb
@@ -1,8 +1,9 @@
 module Neo4j
-
   module Community
     VERSION = "1.6.0.alpha.3"
     NEO_VERSION = "1.6.M02"
   end
-
 end
+
+# Expose the version globally once and here.
+NEO_VERSION = Neo4j::Community::NEO_VERSION
